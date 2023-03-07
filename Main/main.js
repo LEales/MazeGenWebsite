c9e8a58edf2523c5a6c67bacb5d1a25a7e3b3c63
twegestal <96296150+twegestal@users.noreply.github.com>
2023-03-07 11:01:47 +0100

    cool stuff and other stuff and stuff
    Co-Authored-By: LEales <102584475+LEales@users.noreply.github.com>

---
+++ Main/main.js
@@ -0,0 +1,17 @@
+
+$( document ).ready(function() {
+    const items = document.querySelectorAll('.image-container');
+    const zoomOverlay = document.querySelector('.zoom-overlay');
+    const zoomImage = zoomOverlay.querySelector('img');
+    items.forEach(item => {
+        const image = item.querySelector('img');
+        image.addEventListener('click', () => {
+            zoomImage.src = image.src;
+            zoomOverlay.style.display = 'flex';
+        });
+    });
+    zoomOverlay.addEventListener('click', () => {
+        zoomOverlay.style.display = 'none';
+    });
+});
+
