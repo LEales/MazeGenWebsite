9ef044d010a6deb45ffd85521172d3fb6fc1c2c4
twegestal <96296150+twegestal@users.noreply.github.com>
2023-03-10 08:58:15 +0100

    asdasd

--- Main/game.js
+++ Main/game.js
@@ -1,4 +1,38 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
+
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
+
+    const Parts = {
+        Path: "./Resources/Images/Game/path.png",
+        Wall: "./Resources/Images/Game/wall.png",
+        Start: "./Resources/Images/Game/wall.png",
+        Goal: "./Resources/Images/Game/goal.png"
+    }
+
+    const maze = [
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Goal, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Path, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall],
+        [Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Start, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall, Parts.Wall]
+    ];
+    
+
+    var gridSize = 10;
+    var cellSize = canvas.width / gridSize;
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            let image = new Image();
+            image.src = maze[i][j];
+
+            context.drawImage(image, i * cellSize, j * cellSize, cellSize, cellSize);
+        }
+    }
 });
